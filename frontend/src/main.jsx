37de4fde4894fa8678571e86614fab8e364a26c9
slimskhab <slimsskhab@gmail.com>
2024-07-11 21:48:41 +0200

    added form store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,13 +7,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import eventsStore from "./core/Features/Events.js"
 import workshopsStore from "./core/Features/Workshops.js"
 import spacesStore from "./core/Features/Spaces.js"
+import formsStore from "./core/Features/Forms.js"
 
 import { Provider } from "react-redux";
 const store = configureStore({
   reducer: {
     eventsStore: eventsStore,
     workshopsStore:workshopsStore,
-    spacesStore: spacesStore
+    spacesStore: spacesStore,
+    formsStore: formsStore
+
   }
 })
 
