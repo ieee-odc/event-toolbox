44c678c3a3a9661c6b114f2ed2d0e0432f2a27eb
amin <mohamedaminbouali6@gmail.com>
2024-07-22 12:15:43 +0100

    removed impoterd file

--- frontend/src/modules/Form/components/ShareLinkModal.jsx
+++ frontend/src/modules/Form/components/ShareLinkModal.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
-import "./ShareLinkModal.css";
 import { UserData } from "../../../utils/UserData";
 
 
