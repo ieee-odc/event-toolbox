2d6abb0d6592c60c208f2a1723d6c6cdce006aa8
slimskhab <slimsskhab@gmail.com>
2024-08-14 12:11:11 +0100

    feat: Add feature to attach event cover image with blob and firebase storage

--- frontend/src/modules/Participants/pages/CancelRegistration.jsx
+++ frontend/src/modules/Participants/pages/CancelRegistration.jsx
@@ -5,22 +5,31 @@ import {
   base64UrlEncode,
 } from "../../../utils/helpers/base64Helper";
 import axiosRequest from "../../../utils/AxiosConfig";
+import toast from "react-hot-toast";
 
 function CancelRegistration() {
   const { token } = useParams();
   const decodedToken = base64UrlDecode(token);
-  let tokenData;
-  try {
-    tokenData = JSON.parse(decodedToken);
-  } catch (error) {
-    console.error("Invalid token format", error);
-  }
+
+  const [tokenData, setTokenData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    /// eyJwYXJ0aWNpcGFudElkIjoiNjZhYjgyZGRmMjMxZmFlZTM3ODM5Y2VkIiwiZXZlbnRJZCI6Mn0
+    try {
+      setTokenData(JSON.parse(decodedToken));
+    } catch (error) {
+      console.error("Invalid token format", error);
+    }
+  }, [token]);
 
   const [participations, setParticipations] = useState([]);
-  const [eventParticipation, setEventParticipation] = useState({});
+  const [eventParticipation, setEventParticipation] = useState(null);
   const [selectedParticipations, setSelectedParticipations] = useState([]);
 
   useEffect(() => {
+    if (!tokenData) return;
+
+    setLoading(true);
     axiosRequest
       .post("/participant/cancelation-data", {
         participantId: tokenData.participantId,
@@ -35,8 +44,11 @@ function CancelRegistration() {
           (participation) => !participation.workshopId
         );
         setEventParticipation(eventOnlyParticipation || {});
+      })
+      .finally(() => {
+        setLoading(false);
       });
-  }, [token]);
+  }, [tokenData]);
 
   const handleCheckboxChange = (participationId) => {
     setSelectedParticipations((prevSelected) =>
@@ -44,6 +56,51 @@ function CancelRegistration() {
         ? prevSelected.filter((id) => id !== participationId)
         : [...prevSelected, participationId]
     );
+  };
+  const handleCancelWholeEvent = async () => {
+    setLoading(true);
+    try {
+      await axiosRequest.post(
+        `/participant/cancel-event/${tokenData.eventId}`,
+        {
+          participantEmail: eventParticipation.email,
+        }
+      );
+      toast.success("Event registration cancelled successfully.");
+
+      setEventParticipation(null);
+    } catch (error) {
+      console.error("Error cancelling event registration", error);
+      toast.error(
+        "Failed to cancel event registration. Please try again later."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCancelSelectedWorkshops = async () => {
+    setLoading(true);
+    try {
+      console.log(selectedParticipations);
+      const cancelPromises = selectedParticipations.map((participationId) =>
+        axiosRequest.post(`/participant/cancel-workshop/${participationId}`)
+      );
+      await Promise.all(cancelPromises);
+      setParticipations((prevParticipations) =>
+        prevParticipations.filter(
+          (participation) => !selectedParticipations.includes(participation._id)
+        )
+      );
+
+      // Clear the selected participations
+      setSelectedParticipations([]);
+      toast.success("Selected workshop registrations cancelled successfully.");
+    } catch (error) {
+      toast.error("Failed to cancel selected workshop registrations.");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -61,43 +118,83 @@ function CancelRegistration() {
         <div className="authentication-wrapper authentication-basic container-p-y">
           <div className="authentication-inner">
             <div className="card">
-              <div className="card-body">
-                <h4 className="mb-2">Cancel Registration</h4>
-                <p className="mb-6">You can cancel your registration here.</p>
+              {loading ? (
+                <span>Loading</span>
+              ) : (
+                <div className="card-body">
+                  <h4 className="mb-2">Cancel Registration</h4>
+                  {!eventParticipation ? (
+                    <p>Event Participation already canceled</p>
+                  ) : (
+                    <div>
+                      <p className="mb-6">
+                        You can cancel your registration here.
+                      </p>
 
-                <div>
-                  <h5>Cancel the whole event</h5>
-                  <button>Cancel</button>
-                </div>
-
-                <h5>Cancel workshops</h5>
-                {participations.map((participation) => (
-                  <div
-                    key={participation.id}
-                    style={{ display: "flex", alignItems: "center" }}
-                  >
-                    <input
-                      type="checkbox"
-                      id={`workshop-${participation.id}`}
-                      checked={selectedParticipations.includes(
-                        participation.id
+                      <div
+                        style={{
+                          marginBottom: 10,
+                          display: "flex",
+                          flexDirection: "column",
+                          gap: "5px",
+                        }}
+                      >
+                        <h5>Cancel the whole event</h5>
+                        <button
+                          id="deleteButton"
+                          onClick={handleCancelWholeEvent}
+                        >
+                          <p className="mx-1">Cancel</p>
+                          <div className="centered">
+                            {" "}
+                            <i className="bx bxs-trash mx-1"></i>
+                          </div>
+                        </button>
+                      </div>
+                      {participations.length !== 0 && (
+                        <div>
+                          <h5>Cancel workshops</h5>
+                          {participations.map((participation) => (
+                            <div
+                              key={participation.id}
+                              style={{
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "5px",
+                              }}
+                            >
+                              <input
+                                type="checkbox"
+                                id={`workshop-${participation._id}`}
+                                checked={selectedParticipations.includes(
+                                  participation._id
+                                )}
+                                onChange={() =>
+                                  handleCheckboxChange(participation._id)
+                                }
+                              />
+                              <label htmlFor={`workshop-${participation.id}`}>
+                                {participation.workshop.name}
+                              </label>
+                            </div>
+                          ))}
+                          <button
+                            id="deleteButton"
+                            className="w-100"
+                            onClick={handleCancelSelectedWorkshops}
+                          >
+                            <p className="mx-1">Confirm Cancellation</p>
+                            <div className="centered">
+                              {" "}
+                              <i className="bx bxs-trash mx-1"></i>
+                            </div>
+                          </button>
+                        </div>
                       )}
-                      onChange={() => handleCheckboxChange(participation.id)}
-                    />
-                    <label htmlFor={`workshop-${participation.id}`}>
-                      {participation.workshopId}
-                    </label>
-                  </div>
-                ))}
-
-                <button
-                  onClick={() =>
-                    console.log("Selected workshops:", selectedParticipations)
-                  }
-                >
-                  Confirm Cancellation
-                </button>
-              </div>
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
           </div>
         </div>
